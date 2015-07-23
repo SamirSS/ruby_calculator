8f3720d94aea639f601056ecd25efbcfc612b359
Samir Shekhawat <samir.shekhawat4@gmail.com>
2015-07-23 19:42:10 +0000

    Added a continuous total using a while loop

--- calculator.rb
+++ calculator.rb
@@ -1,45 +1,59 @@
-def addition
-  sum = @num1.to_f + @num2.to_f
-  return "The answer is #{sum}"
+def addition(num1, num2)
+  sum = num1.to_f + num2.to_f
+  #return "The answer is #{sum}"
 end
 
-def subtraction
-  diff = @num1.to_f - @num2.to_f
-  return "The answer is #{diff}"
+def subtraction(num1, num2)
+  diff = num1.to_f - num2.to_f
+  #return "The answer is #{diff}"
 end
 
-def multiplication
-  product = @num1.to_f * @num2.to_f
-  return "The answer is #{product}"
+def multiplication(num1, num2)
+  product = num1.to_f * num2.to_f
+  #return "The answer is #{product}"
 end
 
-def division
-  quotient = @num1.to_f / @num2.to_f
-  return "The answer is #{quotient}"
+def division(num1, num2)
+  quotient = num1.to_f / num2.to_f
+  #return "The answer is #{quotient}"
 end
 
-def exponential
-  exp = @num1.to_f ** @num2.to_f
-  return "The answer is #{exp}"
+def exponential(num1, num2)
+  exp = num1.to_f ** num2.to_f
+  #return "The answer is #{exp}"
 end
 
 def simple_calc
-  puts "Enter math equation on separate lines: "
-  @num1 = gets.chomp
-  @sign = gets.chomp
-  @num2 = gets.chomp
-  if @sign == "+"
-    puts addition
-  elsif @sign == "-"
-    puts subtraction
-  elsif @sign == "*"
-    puts multiplication
-  elsif @sign == "/"
-    puts division
-  elsif @sign == "^"
-    puts exponential
-  else
-    puts "Unavailable"
+  total = 0.0
+  on = true
+  
+  while (on == true) do
+    puts "Enter math equation on separate lines: "
+    puts "Your total is #{total}"
+    sign = gets.chomp
+    if sign == "exit"
+      exit
+    end
+    num1 = gets.chomp
+    num1 = num1.to_f
+    if sign == "+"
+      total = total + num1
+      addition(num1, total)
+    elsif sign == "-"
+      total = total - num1
+      subtraction(total, num1)
+    elsif sign == "*"
+      total = total * num1
+      multiplication(num1, total)
+    elsif sign == "/"
+      total = total / num1
+      division(total, num1)
+    elsif sign == "^"
+      total = total ** num1
+      exponential(total, num1)
+    else
+      puts "Unavailable"
+    end
   end
 end
-puts simple_calc
+simple_calc
